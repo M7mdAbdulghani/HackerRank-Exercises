35921973a1595cac3d9ce9663d658194ca23da34
m7mdAbdulghani <mabdulghani98@gmail.com>
2022-09-18 17:29:26 +0300

    BreakingTheRecord

---
+++ HackerRank Exercises/BreakingTheRecord.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank_Exercises
+{
+    public static class BreakingTheRecord
+    {
+        /* 
+            Exercise URL
+            https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem?isFullScreen=true
+
+            var scores = new List<int>();
+
+            //10 5 20 20 4 5 2 25 1
+            scores.Add(10);
+            scores.Add(5);
+            scores.Add(20);
+            scores.Add(20);
+            scores.Add(4);
+            scores.Add(5);
+            scores.Add(2);
+            scores.Add(25);
+            scores.Add(1);
+
+            breakingRecords(scores); 
+        */
+
+        public static List<int> breakingRecords(List<int> scores)
+        {
+            var resultScores = new List<int>();
+            int min = 0;
+            int max = 0;
+
+            int highestScore = 0;
+            int lowestScore = 0;
+
+
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (i == 0)
+                {
+                    highestScore = scores[i];
+                    lowestScore = scores[i];
+                    continue;
+                }
+
+                // Bigger
+                if (scores[i] > highestScore)
+                {
+                    max++;
+                    highestScore = scores[i];
+                }
+
+
+                // Lower
+                if (scores[i] < lowestScore)
+                {
+                    min++;
+                    lowestScore = scores[i];
+                }
+            }
+
+            resultScores.Add(max);
+            resultScores.Add(min);
+
+            return resultScores;
+        }
+    }
+}
--- HackerRank Exercises/Program.cs
+++ HackerRank Exercises/Program.cs
@@ -38,29 +38,6 @@ namespace HackerRank_Exercises
                 Console.WriteLine(minNumber + " " + maxNumber);
             */
 
-            
-
-            
-
-            // Testing Breaking The Records
-            /* 
-                var scores = new List<int>();
-
-                //10 5 20 20 4 5 2 25 1
-                scores.Add(10);
-                scores.Add(5);
-                scores.Add(20);
-                scores.Add(20);
-                scores.Add(4);
-                scores.Add(5);
-                scores.Add(2);
-                scores.Add(25);
-                scores.Add(1);
-
-                breakingRecords(scores); 
-            */
-
-            /******************************************************************************************************************************************************/
 
             // Testing Equalize The array
             /* 
@@ -196,56 +173,6 @@ namespace HackerRank_Exercises
             //Console.WriteLine(catADifference > catBDifference ? "Cat B" : catBDifference > catADifference ? "Cat A" : "Mouse C");
 
         }
-
-
-        
-        
-        
-        // Breaking The Records
-        /* 
-            public static List<int> breakingRecords(List<int> scores)
-            {
-                var resultScores = new List<int>();
-                int min = 0;
-                int max = 0;
-
-                int highestScore = 0;
-                int lowestScore = 0;
-
-            
-                for (int i = 0; i < scores.Count; i++)
-                {
-                    if(i == 0)
-                    {
-                        highestScore = scores[i];
-                        lowestScore = scores[i];
-                        continue;
-                    }
-
-                    // Bigger
-                    if(scores[i] > highestScore)
-                    {
-                        max++;
-                        highestScore = scores[i];
-                    }
-
-
-                    // Lower
-                    if (scores[i] < lowestScore)
-                    {
-                        min++;
-                        lowestScore = scores[i];
-                    }
-                }
-
-                resultScores.Add(max);
-                resultScores.Add(min);
-
-                return resultScores;
-            }  
-        */
-
-        /******************************************************************************************************************************************************/
 
 
         // Equalize The Array (My Solution)
