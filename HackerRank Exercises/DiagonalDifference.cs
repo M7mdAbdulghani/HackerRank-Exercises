90a82d6878ec2f2c747a72ec1a4dce389db777a7
m7mdAbdulghani <mabdulghani98@gmail.com>
2022-09-18 16:51:45 +0300

    Diagonal Difference

---
+++ HackerRank Exercises/DiagonalDifference.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank_Exercises
+{
+    public static class DiagonalDifference
+    {
+        /* 
+            Exercise URL
+            https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
+
+            var numbers = new List<List<int>>();
+
+            numbers.Add(new List<int>());
+            numbers.Add(new List<int>());
+            numbers.Add(new List<int>());
+
+            numbers[0].Add(1);
+            numbers[0].Add(2);
+            numbers[0].Add(3);
+            numbers[1].Add(4);
+            numbers[1].Add(5);
+            numbers[1].Add(6);
+            numbers[2].Add(7);
+            numbers[2].Add(8);
+            numbers[2].Add(9);
+
+            Console.WriteLine(diagonalDifferenceOptimized(numbers)); 
+        */
+
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int leftDialog = 0;
+            int rightDialog = 0;
+
+            for (var i = 0; i < arr.Count; i++)
+            {
+                for (var j = 0; j < arr.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        leftDialog += arr[i][j];
+                    }
+                    if (i + j + 1 == arr.Count)
+                    {
+                        rightDialog += arr[i][j];
+                    }
+                }
+            }
+            return Math.Abs(leftDialog - rightDialog);
+        }
+
+        public static int diagonalDifferenceOptimized(List<List<int>> arr)
+        {
+            int leftDialog = 0;
+            int rightDialog = 0;
+            int count = arr.Count - 1;
+
+
+            for (var i = 0; i < arr.Count; i++)
+            {
+                rightDialog += arr[i][count];
+                leftDialog += arr[i][i];
+                count--;
+            }
+            return Math.Abs(leftDialog - rightDialog);
+        }
+    }
+}
--- HackerRank Exercises/Program.cs
+++ HackerRank Exercises/Program.cs
@@ -9,28 +9,7 @@ namespace HackerRank_Exercises
     {
         static void Main(string[] args)
         {
-            // Testing Diagonal Difference
-            /*
-                var numbers = new List<List<int>>();
-
-                numbers.Add(new List<int>());
-                numbers.Add(new List<int>());
-                numbers.Add(new List<int>());
-
-                numbers[0].Add(1);
-                numbers[0].Add(2);
-                numbers[0].Add(3);
-                numbers[1].Add(4);
-                numbers[1].Add(5);
-                numbers[1].Add(6);
-                numbers[2].Add(7);
-                numbers[2].Add(8);
-                numbers[2].Add(9);
-
-                Console.WriteLine(diagonalDifferenceOptimized(numbers));
-            */
-            /******************************************************************************************************************************************************/
-
+            
             // Testing Compare Triplets 
             /*
                 var numbers = new List<long>();
@@ -310,53 +289,6 @@ namespace HackerRank_Exercises
 
         }
 
-      
-        
-        // Diagonal Difference (My Solution)
-        /*
-                public static int diagonalDifference(List<List<int>> arr)
-                {
-                    int leftDialog = 0;
-                    int rightDialog = 0;
-
-                    for (var i = 0; i < arr.Count; i++)
-                    {
-                        for (var j = 0; j < arr.Count; j++)
-                        {
-                            if (i == j)
-                            {
-                                leftDialog += arr[i][j];
-                            }
-                            if (i + j + 1 == arr.Count)
-                            {
-                                rightDialog += arr[i][j];
-                            }
-                        }
-                    }
-                    return Math.Abs(leftDialog - rightDialog);
-                }
-        */
-
-        // Diagonal Difference (More Optimized Solution)
-        /*
-            public static int diagonalDifferenceOptimized(List<List<int>> arr)
-            {
-                int leftDialog = 0;
-                int rightDialog = 0;
-                int count = arr.Count - 1;
-
-
-                for (var i = 0; i < arr.Count; i++)
-                {
-                    rightDialog += arr[i][count];
-                    leftDialog += arr[i][i];
-                    count--;
-                }
-                return Math.Abs(leftDialog - rightDialog);
-            }
-        */
-
-        /******************************************************************************************************************************************************/
 
         // Time Conversion
         /* 
