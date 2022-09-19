3e28585d7c705a11ccf428112d1a9b5a8b00a8c1
m7mdAbdulghani <mabdulghani98@gmail.com>
2022-09-19 09:55:44 +0300

    SalesByMatch

--- HackerRank Exercises/Program.cs
+++ HackerRank Exercises/Program.cs
@@ -9,60 +9,7 @@ namespace HackerRank_Exercises
     {
         static void Main(string[] args)
         {
-            
-            // Testing Compare Triplets 
-            /*
-                var numbers = new List<long>();
-                // 7 69 2 221 8974
-                numbers.Add(7);
-                numbers.Add(69);
-                numbers.Add(2);
-                numbers.Add(221);
-                numbers.Add(8974);
-                numbers.Sort();
-
-
-                long maxNumber = 0;
-                long minNumber = 0;
-
-                for (int i = 0; i < numbers.Count - 1; i++)
-                {
-                    minNumber += numbers[i];
-                }
-
-                for (int j = 1; j < numbers.Count; j++)
-                {
-                    maxNumber += numbers[j];
-                }
-
-                Console.WriteLine(minNumber + " " + maxNumber);
-            */
-
-
-            
-
-            *****************************
-
-            //List<int> ar = new List<int>() { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
-
-            //int[] occurences = new int[101];
-
-            //int counter = 0;
-            //for (int i = 0; i < ar.Count; i++)
-            //{
-            //    int number = ar[i];
-            //    occurences[number]++;
-
-            //    if(occurences[number] == 2)
-            //    {
-            //        counter++;
-            //        occurences[number] = 0;
-            //    }
-            //}
-            //Console.WriteLine(counter);
-
             //Console.WriteLine(Math.Log2(10));
-
             //Stopwatch stopwatch = new Stopwatch();
             //stopwatch.Start();
 
@@ -77,14 +24,6 @@ namespace HackerRank_Exercises
             //Console.WriteLine(result);
             //stopwatch.Stop();
             //Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
-
-
-            
-
-            
-
         }
-
-
     }
 }
---
+++ HackerRank Exercises/SalesByMatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank_Exercises
+{
+    public static class SalesByMatch
+    {
+        /* 
+            Exercise URL
+            https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true
+
+            List<int> ar = new List<int>() { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+
+            Console.WriteLine(sockMerchant(ar.Count, ar)); 
+        */
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int[] occurences = new int[101];
+            int counter = 0;
+            for (int i = 0; i < ar.Count; i++)
+            {
+                int number = ar[i];
+                occurences[number]++;
+
+                if (occurences[number] == 2)
+                {
+                    counter++;
+                    occurences[number] = 0;
+                }
+            }
+            return counter;
+        }
+    }
+}
