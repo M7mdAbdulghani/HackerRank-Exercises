091c1c7f28e70e3a0f646c98782b50f8fb91b931
m7mdAbdulghani <mabdulghani98@gmail.com>
2022-09-22 18:17:34 +0300

    CircularArrayRotation

---
+++ HackerRank Exercises/CircularArrayRotation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank_Exercises
+{
+    public class CircularArrayRotation
+    {
+        /* 
+            Exercise URL
+            https://www.hackerrank.com/challenges/circular-array-rotation/problem?isFullScreen=true
+
+            var a = new List<int>() { 3, 4, 5 };
+            var k = 2;
+            var queries = new List<int>() { 1, 2 };
+
+            circularArrayRotation(a, k, queries);
+        */
+        public static List<int> circularArrayRotation(List<int> a, int k, List<int> queries)
+        {
+            // Copy
+            var copyArray = a.ToList();
+
+            for (int i = 0; i < copyArray.Count; i++)
+            {
+                var condition = (i + k) % copyArray.Count;
+                a[condition] = copyArray[i];
+            }
+
+            var result = new List<int>();
+            foreach (var index in queries)
+            {
+                result.Add(a[index]);
+            }
+
+            return result;
+        }
+    }
+}
